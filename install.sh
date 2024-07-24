30a8346bbea9adbd970a2fad73a9039a5999304d
yuu-senoo <kahunsho0@gmail.com>
2024-07-24 23:24:12 +0900

    .zshrcの追加

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+IGNORE_PATTERN="^\.(git|travis)"
+
+echo "Create dotfile links."
+for dotfile in .??*; do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+done
+echo "Success"
